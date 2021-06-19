fb1d496ef180b98941c87a88521c0f14038594ba
Jules <68038306+awesomej126@users.noreply.github.com>
2021-06-19 19:40:54 -0400

    Update eyeshadow.py

--- eyeshadow.py
+++ eyeshadow.py
@@ -1,9 +1,14 @@
 import random
+import tkinter
+
+window = tkinter.Tk()
+window.title("Eyeshadow Randomizer")
+window.geometry("800x300")
 
 def palettes():
     palette_list = ["James Charles Palette", "Jaclyn Hill Palette"]
     palette = random.choices(palette_list)
-    print("Eyeshadow palette: " + str(palette))
+    palette_result["text"] = "Eyeshadow palette: " + str(palette)
 
     if palette == ["James Charles Palette"]:
         james_mattes = ["Canvas", "518", "Rusted", "Tea", "Punch Me", "Mary", "You're Kidding", "Boutique", "Benny", "Flashback", "Tune", "Code James", "10% Off", "No Beans", "Spooky", "Bee", "Playground", "Brother", "Love That", "Pinkity Drinkity", "Social Blade", "Daddy", "Cola", "Escape", "Single", "Skip"]
@@ -13,7 +18,7 @@ def palettes():
         outer_v1 = random.choices(james_mattes)
         lid1 = random.choices(james_shimmers)
         lower_lash_line1 = random.choices(james_mattes)
-        print("Transition color: " + str(transition1) + ", Crease color: " + str(crease1) + ", Outer-V color: " + str(outer_v1) + ", Lid Color: " + str(lid1) + ", Lower Lash Line Color: " + str(lower_lash_line1))
+        shades_result["text"] = "Transition color: " + str(transition1) + ", Crease color: " + str(crease1) + ", Outer-V color: " + str(outer_v1) + ", Lid Color: " + str(lid1) + ", Lower Lash Line Color: " + str(lower_lash_line1)
     elif palette == ["Jaclyn Hill Palette"]:
         jaclyn_mattes = ["Silk Creme", "M.F.E.O.", "Creamsicle", "Butter", "Pooter", "Pukey", "Hunts", "Roxanne", "Jacz", "Chip", "Mocha", "Jada", "Enchanted", "Central Park", "Soda Pop", "Abyss"]
         jaclyn_shimmers = ["Enlight", "Beam", "Faint", "Sissy", "Little Lady", "Firework", "Queen", "Obsessed", "S.B.N.", "Hillster", "Buns", "Cranapple", "Royalty", "Twerk", "Hustle", "Meeks", "24/7", "Pool Party", "Diva"]
@@ -22,18 +27,37 @@ def palettes():
         outer_v2 = random.choices(jaclyn_mattes)
         lid2 = random.choices(jaclyn_shimmers)
         lower_lash_line2 = random.choices(jaclyn_mattes)
-        print("Transition color: " + str(transition2) + ", Crease color: " + str(crease2) + ", Outer-V color: " + str(outer_v2) + ", Lid Color: " + str(lid2) + ", Lower Lash Line Color: " + str(lower_lash_line2))
+        shades_result["text"] = "Shades: Transition color: " + str(transition2) + ", Crease color: " + str(crease2) + ", Outer-V color: " + str(outer_v2) + ", Lid Color: " + str(lid2) + ", Lower Lash Line Color: " + str(lower_lash_line2)
 
 def type():
     types = ["Halo", "Cut Crease", "Half Cut Crease", "Smokey Eye"]
     type = random.choices(types)
-    print("Eyeshadow type: " + str(type))
+    type_result["text"] = "Eyeshadow type: "+ str(type)
 
 def liner():
     eyeliner = ["Winged", "Smudged", "Fox Eye"]
     style = random.choices(eyeliner)
-    print("Eyeliner style: " + str(style))
+    liner_result["text"] = "Eyeliner style: " + str(style)
 
-palettes()
-type()
-liner()
+palette_button = tkinter.Button(window, text="Palette", command=palettes)
+palette_button.pack()
+
+palette_result = tkinter.Label(window, text="")
+palette_result.pack()
+
+shades_result = tkinter.Label(window, text="")
+shades_result.pack()
+
+type_button = tkinter.Button(window, text="Type", command=type)
+type_button.pack()
+
+type_result = tkinter.Label(window, text="")
+type_result.pack()
+
+liner_button = tkinter.Button(window, text="Eyeliner", command=liner)
+liner_button.pack()
+
+liner_result = tkinter.Label(window, text="")
+liner_result.pack()
+
+window.mainloop()
