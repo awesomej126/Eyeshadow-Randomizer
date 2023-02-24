a33980f5cf3e2098131a94a09ebaa5df053514c5
awesomej126 <68038306+awesomej126@users.noreply.github.com>
2023-02-23 22:53:58 -0500

    Changed one function, added another
    Added palette_selector function to randomly choose palette, and modified palettes function to choose colors and add palette picture and name to GUI.

--- Makeup Randomizers/Eyeshadow/eyeshadow.py
+++ Makeup Randomizers/Eyeshadow/eyeshadow.py
@@ -9,55 +9,54 @@ window.title("Eyeshadow Randomizer")
 window.geometry("1000x500")
 
 # Palette Images
-jh_image = ImageTk.PhotoImage(Image.open('Eyeshadow/jh_palette.jpg').resize((350, 200)))
-jc_image = ImageTk.PhotoImage(Image.open('Eyeshadow/jc_palette.jpg').resize((250, 200)))
-mickey_image = ImageTk.PhotoImage(Image.open('Eyeshadow/mickey_mouse.jpg').resize((250, 200)))
-q_image = ImageTk.PhotoImage(Image.open('Eyeshadow/9Q.jpg').resize((250, 200)))
+jh_image = ImageTk.PhotoImage(Image.open('jh_palette.jpg').resize((350, 200)))
+jc_image = ImageTk.PhotoImage(Image.open('jc_palette.jpg').resize((250, 200)))
+mickey_image = ImageTk.PhotoImage(Image.open('mickey_mouse.jpg').resize((250, 200)))
+q_image = ImageTk.PhotoImage(Image.open('9Q.jpg').resize((250, 200)))
 
-# Shade Names
-james_mattes = ["Canvas", "518", "Rusted", "Tea", "Punch Me", "Mary", "You're Kidding", "Boutique", "Benny", "Flashback", "Tune", "Code James", "10% Off", "No Beans", "Spooky", "Bee", "Playground", "Brother", "Love That", "Pinkity Drinkity", "Social Blade", "Daddy", "Cola", "Escape", "Single", "Skip"]
-james_shimmers = ["Ringlight", "So Good", "Halloween", "Wig", "Sister", "Literally", "Shook", "Face", "Guac", "Hello", "Artistry", "Cape Cod", "A Capella"]
-jaclyn_mattes = ["Silk Creme", "M.F.E.O.", "Creamsicle", "Butter", "Pooter", "Pukey", "Hunts", "Roxanne", "Jacz", "Chip", "Mocha", "Jada", "Enchanted", "Central Park", "Soda Pop", "Abyss"]
-jaclyn_shimmers = ["Enlight", "Beam", "Faint", "Sissy", "Little Lady", "Firework", "Queen", "Obsessed", "S.B.N.", "Hillster", "Buns", "Cranapple", "Royalty", "Twerk", "Hustle", "Meeks", "24/7", "Pool Party", "Diva"]
-mickey_mattes = ["Head Out", "Life's Short", "So Fearless", "Big Dreamer", "Play by Ear", "Stop & Stare", "Big Bows", "Coy Toy", "On the Dot", "Bat Those Lashes", "Awe-Dascious", "Awe Goody", "Out Loud", "Very Vixen", "Thumbs Up", "Stun & Games", "Street Beats", "Major Edge", "Secret Weapon", "Original Artist", "Push It", "Max Power", "Wild One", "Instigator", "Total Rebel"]
-mickey_shimmers = ["Own It", "Creative Collision", "Made Ya Look", "Extra Sparkle", "Staring Contest", "Super Sassy", "Talk Flirty", "Iconic Vibes", "Troublemaker", "Into Mischief"]
-q_shades = ["Life", "Healing", "Sun", "Spirit", "Fluidity", "Nature", "Representation", "Serenity", "Visibilty"]
+# Shade Names (Made them tuples cause the shade names stay constant)
+jc_mattes = ("Canvas", "518", "Rusted", "Tea", "Punch Me", "Mary", "You're Kidding", "Boutique", "Benny", "Flashback", "Tune", "Code James", "10% Off", "No Beans", "Spooky", "Bee", "Playground", "Brother", "Love That", "Pinkity Drinkity", "Social Blade", "Daddy", "Cola", "Escape", "Single", "Skip")
+jc_shimmers = ("Ringlight", "So Good", "Halloween", "Wig", "Sister", "Literally", "Shook", "Face", "Guac", "Hello", "Artistry", "Cape Cod", "A Capella")
+jh_mattes = ("Silk Creme", "M.F.E.O.", "Creamsicle", "Butter", "Pooter", "Pukey", "Hunts", "Roxanne", "Jacz", "Chip", "Mocha", "Jada", "Enchanted", "Central Park", "Soda Pop", "Abyss")
+jh_shimmers = ("Enlight", "Beam", "Faint", "Sissy", "Little Lady", "Firework", "Queen", "Obsessed", "S.B.N.", "Hillster", "Buns", "Cranapple", "Royalty", "Twerk", "Hustle", "Meeks", "24/7", "Pool Party", "Diva")
+mickey_mattes = ("Head Out", "Life's Short", "So Fearless", "Big Dreamer", "Play by Ear", "Stop & Stare", "Big Bows", "Coy Toy", "On the Dot", "Bat Those Lashes", "Awe-Dascious", "Awe Goody", "Out Loud", "Very Vixen", "Thumbs Up", "Stun & Games", "Street Beats", "Major Edge", "Secret Weapon", "Original Artist", "Push It", "Max Power", "Wild One", "Instigator", "Total Rebel")
+mickey_shimmers = ("Own It", "Creative Collision", "Made Ya Look", "Extra Sparkle", "Staring Contest", "Super Sassy", "Talk Flirty", "Iconic Vibes", "Troublemaker", "Into Mischief")
+q_shades = ("Life", "Healing", "Sun", "Spirit", "Fluidity", "Nature", "Representation", "Serenity", "Visibilty")
 
 # Functions
-# Chooses your palette and colors
-def palettes():
-    palette_button["state"] = "disabled"
-    palettes = ["James Charles Palette", "Jaclyn Hill Palette", "Mickey & Friends Palette", "9Q Love Matters Palette"]
-    palette = random.choice(palettes)
-    palette_result["text"] = "Eyeshadow palette: {0}".format(palette)
-
+# Associates palette with its picture and shades, chooses shades
+def palettes(palette_name, palette_image, mattes, shimmers=()):
+    palette_pic["image"] = palette_image
+    palette_result["text"] = "Eyeshadow palette: {0}".format(palette_name)
+    m = mattes
+    if shimmers != ():
+        s = shimmers
+    
     mnum = random.randint(2, 4)
     snum = random.randint(0, 2)
 
-    if palette == "James Charles Palette":
-        palette_pic["image"] = jc_image
-        m = james_mattes
-        s = 0 if snum == 0 else james_shimmers
+    selected_mattes = ", ".join(random.choices(m, k=mnum))
+    selected_shimmers = "None" if snum == 0 else ", ".join(random.choices(s, k=snum))
 
-    elif palette == "Jaclyn Hill Palette":
-        palette_pic["image"] = jh_image
-        m = jaclyn_mattes
-        s = 0 if snum == 0 else jaclyn_shimmers
+    shades_result["text"] = "Matte Shades: {}; Shimmer Shades: {}".format(selected_mattes, selected_shimmers)
 
-    elif palette == "Mickey & Friends Palette":
-        palette_pic["image"] = mickey_image
-        m = mickey_mattes
-        s = 0 if snum == 0 else mickey_shimmers
+# Chooses your palette, colors determined by palette_selector
+def palette_selector():
+    palette_button["state"] = "disabled"
+    palette_names = ["James Charles Palette", "Jaclyn Hill Palette", "Mickey & Friends Palette", "9Q Love Matters Palette"]
+    selected_palette = random.choice(palette_names)
+
+    if selected_palette == "James Charles Palette":
+        palettes(selected_palette, jc_image, jc_mattes, jc_shimmers)
+
+    elif selected_palette == "Jaclyn Hill Palette":
+        palettes(selected_palette, jh_image, jh_mattes, jh_shimmers)
+
+    elif selected_palette == "Mickey & Friends Palette":
+        palettes(selected_palette, mickey_image, mickey_mattes, mickey_shimmers)
     
-    elif palette == "9Q Love Matters Palette":
-        palette_pic["image"] = q_image
-        m = q_shades
-        snum = 0
-
-    mattes = ", ".join(random.choices(m, k=mnum))
-    shimmers = "None" if snum == 0 else ", ".join(random.choices(s, k=snum))
-
-    shades_result["text"] = "Matte Shades: {}; Shimmer Shades: {}".format(mattes, shimmers)
+    elif selected_palette == "9Q Love Matters Palette":
+        palettes(selected_palette, q_image, q_shades)
 
 # Chooses the style of eyeshadow
 def eyeshadow_type():
@@ -80,7 +79,7 @@ def reset():
 
 
 # Buttons
-palette_button = tkinter.Button(window, text="Palette", command=palettes)
+palette_button = tkinter.Button(window, text="Palette", command=palette_selector)
 palette_button.pack()
 
 palette_pic = tkinter.Label(window, image="")
